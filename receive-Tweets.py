8c2e4a3439b6a4389b705468bf5ab8e6b942b6fc
Nasir Safdari <nxs5899@rit.edu>
2019-02-11 10:07:07 -0500

    adding files to the repo

---
+++ receive-Tweets.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import tweepy
+from tweepy.auth import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import socket
+import json
+
+# request to get credentials at http://apps.twitter.com
+consumer_key    = 'sC0EdC23CJdSIY9uR2aLB8vO7'
+consumer_secret = 'jgmHwkeZM9eGe06hzm00bQlxNcuJzl3NI5woKm9h6o37Qh2Qf2'
+access_token    = '1049354488547500032-N4htfIYjXBZvNBBa0BFw24Ixhc1kLE'
+access_secret   = 'ziNKjwDwTT4aneUyLz4ILVjhc83H9Dw6GPbXJuS1QufAE'
+
+# we create this class that inherits from the StreamListener in tweepy StreamListener
+class TweetsListener(StreamListener):
+
+    def __init__(self, csocket):
+        self.client_socket = csocket
+    # we override the on_data() function in StreamListener
+    def on_data(self, data):
+        try:
+            message = json.loads( data )
+            print( message['text'].encode('utf-8') )
+            self.client_socket.send( message['text'].encode('utf-8') )
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def if_error(self, status):
+        print(status)
+        return True
+
+
+def send_tweets(c_socket):
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    
+    twitter_stream = Stream(auth, TweetsListener(c_socket))
+    twitter_stream.filter(track=['football'])
+
+
+
+if __name__ == "__main__":
+    new_skt = socket.socket()         # initiate a socket object
+    host = "127.0.0.1"     # local machine address
+    port = 5555                 # specific port for your service.
+    new_skt.bind((host, port))        # Binding host and port
+
+    print("Now listening on port: %s" % str(port))
+
+    new_skt.listen(5)                 #  waiting for client connection.
+    c, addr = new_skt.accept()        # Establish connection with client. it returns first a socket object,c, and the address bound to the socket
+
+    print("Received request from: " + str(addr))
+    # and after accepting the connection, we aill sent the tweets through the socket
+    send_tweets(c)
+
+
+
+
+
+
